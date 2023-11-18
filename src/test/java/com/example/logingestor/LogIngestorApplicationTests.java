51dde273ef3f2836af2b695922ab8fa62c73e69e
verma-SID <siddhanthverma1412@gmail.com>
2023-11-18 11:52:33 +0530

    initialized

---
+++ src/test/java/com/example/logingestor/LogIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
